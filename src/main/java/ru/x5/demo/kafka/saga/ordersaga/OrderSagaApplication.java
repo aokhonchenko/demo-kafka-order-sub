b2f2d50a5c9e9ee86016f64b56db4a6178496214
aokhonchenko <aleksand.okhonchenko@x5.ru>
2022-12-11 11:27:41 +0300

    init commit

---
+++ src/main/java/ru/x5/demo/kafka/saga/ordersaga/OrderSagaApplication.java
@@ -0,0 +1,13 @@
+package ru.x5.demo.kafka.saga.ordersaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSagaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSagaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/x5/demo/kafka/saga/ordersaga/OrderSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.x5.demo.kafka.saga.ordersaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSagaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
